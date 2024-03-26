5d19f32e1b34f124dd4500274a68d4c2ccd77fc9
bochajazz <32917301+bochajazz@users.noreply.github.com>
2024-03-26 20:32:28 -0300

    Add files via upload
    Ejemplo de funciones para simulacro de examen ingreso tecnicatura en desarrollo de software.

---
+++ Simulacro/ej.js
@@ -0,0 +1,15 @@
+
+
+function condicion (nota){
+
+       
+    if (nota < 4){
+    console.log("Tu condición es Libre");
+} if (nota < 7){
+    console.log("Tu condición es regular y no libre");
+}
+
+else {console.log("Tu condición es promocional");}
+}
+
+condicion(4);
