80af17515c5f619d98469eae0f116d3b8e40d7dd
bochajazz <32917301+bochajazz@users.noreply.github.com>
2024-03-26 21:12:28 -0300

    Add files via upload
    actualización parte tres.

--- Simulacro/ej.js
+++ Simulacro/ej.js
@@ -5,11 +5,44 @@ function condicion (nota){
        
     if (nota < 4){
     console.log("Tu condición es Libre");
-} if (nota < 7){
-    console.log("Tu condición es regular y no libre");
+} 
+    if (nota < 7){
+    console.log("Tu condición no es libre es regular");
 }
 
 else {console.log("Tu condición es promocional");}
+
+
+//return console.log("El valor expresado no corresponde a una nota");
+
+//return (if nota <= 11)
+//console.log("El valor expresado no corresponde a una nota");
 }
 
-condicion(4);
+condicion(1);
+
+console.log("-----tercera parte del ejercicio------");
+
+function promedio (num1, num2, num3){
+
+    let num1 = 3;
+    let num2 = 6;
+    let num3 = 8;
+       
+    if (nota < 4){
+    console.log("Tu condición es Libre");
+} 
+    if (nota < 7){
+    console.log("Tu condición no es libre es regular");
+}
+
+else {console.log("Tu condición es promocional");}
+
+
+//return console.log("El valor expresado no corresponde a una nota");
+
+//return (if nota <= 11)
+//console.log("El valor expresado no corresponde a una nota");
+}
+
+condicion(1);
