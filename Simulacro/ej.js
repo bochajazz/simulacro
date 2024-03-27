15690b406601f1cf0ce4d4e0b916cc14baffda73
bochajazz <32917301+bochajazz@users.noreply.github.com>
2024-03-26 21:18:51 -0300

    Add files via upload
    cálculo de promedio

--- Simulacro/ej.js
+++ Simulacro/ej.js
@@ -23,16 +23,21 @@ condicion(1);
 
 console.log("-----tercera parte del ejercicio------");
 
+
+let num1 = 3;
+let num2 = 6;
+let num3 = 8;
+
+let resultado =(num1 + num2 + num3) / 3;
+
 function promedio (num1, num2, num3){
 
-    let num1 = 3;
-    let num2 = 6;
-    let num3 = 8;
-       
-    if (nota < 4){
+   
+    console.log(resultado);
+    if (resultado < 4){
     console.log("Tu condición es Libre");
 } 
-    if (nota < 7){
+    if (resultado < 7){
     console.log("Tu condición no es libre es regular");
 }
 
@@ -45,4 +50,4 @@ else {console.log("Tu condición es promocional");}
 //console.log("El valor expresado no corresponde a una nota");
 }
 
-condicion(1);
+promedio();
